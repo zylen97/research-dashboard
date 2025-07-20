ed612008924033ccae674e16ff35ad0d0faf5b68
Zylen Wang <zylenw97@gmail.com>
2025-07-20 21:22:29 +0800

    fix: Remove username and password length restrictions
    - Remove 3-character minimum for username
    - Remove 6-character minimum for password
    - Allow shorter usernames (zl, zz, yq, dz) and passwords (123)
    
    This enables the new user credentials to work properly.

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -43,8 +43,7 @@ const LoginForm: React.FC = () => {
           name="username"
           label="用户名"
           rules={[
-            { required: true, message: '请输入用户名' },
-            { min: 3, message: '用户名至少3个字符' }
+            { required: true, message: '请输入用户名' }
           ]}
         >
           <Input
@@ -58,8 +57,7 @@ const LoginForm: React.FC = () => {
           name="password"
           label="密码"
           rules={[
-            { required: true, message: '请输入密码' },
-            { min: 6, message: '密码至少6个字符' }
+            { required: true, message: '请输入密码' }
           ]}
         >
           <Input.Password
